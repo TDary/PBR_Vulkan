9650ac19b02aad2e14b64b64e4c42653490d21ea
chenderui <chenderui1@thewesthill.net>
2026-06-05 14:41:52 +0800

    提交代码工程

---
+++ MyPBR.cpp
@@ -0,0 +1,24 @@
+﻿// MyPBR.cpp: 定义应用程序的入口点。
+//
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "MyPBR.h"
+
+using namespace std;
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "MyPBR", nullptr, nullptr);
+	// 创建vulkan实例、设备、交换链...全部从这里开始
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		// 渲染循环
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
