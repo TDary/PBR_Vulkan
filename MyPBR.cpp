3a42d7f06c509f78e25f30eed462e9a6f72084ad
chenderui <chenderui1@thewesthill.net>
2026-06-09 16:13:04 +0800

    提交一波修改

--- MyPBR.cpp
+++ MyPBR.cpp
@@ -2,23 +2,83 @@
 //
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <iostream>
+#include <vulkan/vulkan.h>
+#include <stdexcept>
+#include <cstdlib>
 #include "MyPBR.h"
-
 using namespace std;
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication
+{
+public:
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+private:
+	GLFWwindow* window;
+	VkInstance instance;
+	void initWindow()
+	{
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "My Vulkan", nullptr, nullptr);
+	}
+	void initVulkan()
+	{
+		createInstance();
+	}
+	void createInstance()
+	{
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(0,1,0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+		
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+	}
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
+	void cleanup()
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+};
 
 int main()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "MyPBR", nullptr, nullptr);
-	// 创建vulkan实例、设备、交换链...全部从这里开始
-	while (!glfwWindowShouldClose(window))
+	HelloTriangleApplication app;
+	try
 	{
-		glfwPollEvents();
-		// 渲染循环
+		app.run();
 	}
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	return 0;
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
